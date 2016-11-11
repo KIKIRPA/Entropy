fb194728e2c508c5f821af5cf954e3c01d7d909b
kobbejager <wfremout+github@gmail.com>
2016-11-11 11:30:35 +0100

    new module_landingpage

---
+++ inc/module_landingpage.inc.php
@@ -0,0 +1,16 @@
+<?php
+  // prevent direct access to this file (thus only when included)
+  if (count(get_included_files()) == 1) 
+  {
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+    header("Status: 404 Not Found");
+    exit("Direct access not permitted.");
+  }
+
+  $lp = true;  // let module_libedit know which data it should expect
+  include('./inc/module_libedit.inc.php');
+?>
+
+  
+  
+  
