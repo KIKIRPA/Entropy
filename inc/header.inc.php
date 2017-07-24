c2af1d39f4a8bb707113a1bca4b8459af435a1e5
kobbejager <wfremout+github@gmail.com>
2017-07-24 22:15:05 +0200

    introducing bulma and font awesome (via cdn)

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -47,6 +47,8 @@
     <meta http-equiv="Window-target" content="_top" />
     <meta name="keywords" content="<?php echo $htmlkeywords; ?>"/>
     <link rel="shortcut icon" href="./img/entropy.png" type="image/x-icon" />
+    <link rel="stylesheet" type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.4/css/bulma.min.css'>
     <link rel='stylesheet' type='text/css' href='./css/general.css'>
     <link rel='stylesheet' type='text/css' href='./css/navigation.css'>
     <link rel='stylesheet' type='text/css' href='./css/jquery.notifyBar.css'>
