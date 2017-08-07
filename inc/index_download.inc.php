b4901eec739c0658137c94393c1b9741aa74e675
kobbejager <wfremout+github@gmail.com>
2017-08-07 19:13:31 +0200

    download: actual file download code. Closes #5
    No file conversion yet. Needs to be tested!!

--- inc/index_download.inc.php
+++ inc/index_download.inc.php
@@ -12,7 +12,7 @@
     I. conditions
    *************** 
    
-   1. evaluate $_REQUEST
+   1. evaluate $_REQUEST[lib & id & ds]   --> TODO: move from 
    2. evaluate download code 
    3. evaluate download logging (via login, cookie or form)
    => if conditions are not met, fallback to view module, with error notification
@@ -40,28 +40,29 @@
     // 3. evaluate download logging (via login, cookie or form)
     if (LOG_DL)
     {
-      if (isset($_COOKIE[COOKIE_NAME]))
+      if ($is_logged_in)   
+        $log = array($USERS[$is_logged_in]["name"], $USERS[$is_logged_in]["institution"], $USERS[$is_logged_in]["email"], "login");
+      elseif (isset($_COOKIE[COOKIE_NAME]))
       {
-        if (!verifycookie($_COOKIE[COOKIE_NAME]))
+        if (verifycookie($_COOKIE[COOKIE_NAME]))
+          $log = array($USERS[$is_logged_in]["name"], $USERS[$is_logged_in]["institution"], $USERS[$is_logged_in]["email"], "cookie");
+        else
         {
           removecookie();   // remove invalid cookie
           throw new Exception("Download failed: invalid cookie.");
         }
       }
-      elseif (    isset($_REQUEST["cookie"])
-              and isset($_REQUEST["name"]) 
-              and isset($_REQUEST["institution"]) 
-              and isset($_REQUEST["email"]) )
+      elseif ( isset($_REQUEST["name"]) and isset($_REQUEST["institution"]) and isset($_REQUEST["email"]) )
       {
         $cookie = verifycookie($_REQUEST["name"], $_REQUEST["institution"], $_REQUEST["email"]);
         if ($cookie)
         {
+          $log = array($USERS[$is_logged_in]["name"], $USERS[$is_logged_in]["institution"], $USERS[$is_logged_in]["email"], "form");
           if (isset($_REQUEST["cookie"])) $cookie = makecookie($cookie); // set cookie, if the user checked the checkbox
         }
         else throw new Exception("Download failed: invalid name, institution or e-mail address.");
       }
-      elseif (!$is_logged_in)                         
-        throw new Exception("Download failed: no identification.");
+      else throw new Exception("Download failed: no identification.");
     }
   }
   catch (Exception $e)
@@ -74,44 +75,70 @@
   }
 
 
-  try 
-  {
-   
-  }
-  catch (Exception $e)
-  {
-    $errormsg = $e->getMessage();
-    eventLog("WARNING", $errormsg  . " [index]");
-    echo "    <span style='color:red'>WARNING: " . $errormsg . "</span><br><br>";
-    $mode = "view";
-  }
-
-
-
-
-
-
-
-
 
 /* ******************
     II. calculations
-   ****************** */
+   ****************** 
+   
+   1. download log (if enabled)
+   2. prepare download
+*/
 
-   if ($code[1] == "conv")
-   {
+  // 1. download log (if enabled)
+  // CSV columns: timestamp | name | institution | email | source | IP | library | measurement | dataset | conv/bin | format/target | RESERVED* 
+  //  (*) reserved for (optional) conversion rules (eg TXT: tabulated, comma separated, eg SPC: old format), stored in $code[2]  --> TODO
+  if (LOG_DL)
+  {
+    array_unshift($log, date('Y-m-d H:i'));
+    array_push(   $log, $_SERVER['REMOTE_ADDR'], $showlib, $showid, $showds, $code[0], $code[1], "");
+    
+    // open or create download.csv and append line
+    $handle = fopen(LOG_DL_FILE, "a");
+    if ($handle)  $success = fputcsv($handle, $log);
+    else 	        $success = false;
+    if ($success) $success = fclose($handle);
+    
+    if (!$success) 
+      eventLog("WARNING", "could not write to download log [download]", false, true);
+  }
 
-   }
-   elseif ($code[1] == "bin")
-   {
+  // 2. prepare download
+  if ($code[0] == "conv")
+  {
+    //TODO: integrate convert-framework (check if we are able to convert to $format, preferably replacing the switch by a function)
+    //$export = export($data["dataset"][$ds], $code, $EXPORT)
+    $filename = $showid . (($showds == 'default')?"":"__".$showds) . "." . $code[1];
+    // TODO: code[1] should be the file extension!
+    exit();
+  }
+  elseif ($code[0] == "bin")
+    $filename = $code[1];
 
-   }
 
 
 /* *************
     III. output
    ************* */
 
+  header('Content-Description: File Transfer');
+  header('Content-Type: application/octet-stream');
+  header('Content-Disposition: attachment; filename="'.$filename.'"');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+
+  if ($code[0] == "bin")
+  {
+    header('Content-Length: ' . filesize($code[1]));
+    readfile($code[1]);
+  }
+  elseif ($code[0] == "conv")
+  {
+    header('Content-Length: ' . strlen($export));
+    echo $export;
+  }
+
+  exit;
 
 
 
