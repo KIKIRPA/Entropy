5e51302b41ed539126b6a5e03f05e102b075503d
kobbejager <wfremout+github@gmail.com>
2017-09-04 21:21:06 +0200

    bugfix

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once('install.conf.php');
-require_once(PRIVPATH . 'inc/entropy.conf.php');
+require_once(PRIVPATH . 'entropy.conf.php');
 require_once(PRIVPATH . 'inc/init.inc.php');
 require_once(PRIVPATH . 'inc/common_basic.inc.php');
 require_once(PRIVPATH . 'inc/common_cookie.inc.php');
