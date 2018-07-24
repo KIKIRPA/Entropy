108a8238ccdd5489cec53b72fd694c1c029cf7ce
kobbejager <wfremout+github@gmail.com>
2018-07-24 18:39:24 +0200

    fix regression: mailhider.js is called multiple times

--- app/inc/header.inc.php
+++ app/inc/header.inc.php
@@ -138,8 +138,8 @@ unset($lib, $id, $value, $perm);
 <?php endforeach; ?>
 <?php foreach ($htmlHeaderScripts as $value): ?>
         <script type='text/javascript' src="<?= $value ?>"></script>
-        <script type='text/javascript' src="./js/mailhider.js"></script>
 <?php endforeach; ?>
+        <script type='text/javascript' src="./js/mailhider.js"></script>
     </head>
   
     <body>
