c7cb11af6875e3a9475c29f30894de0791a7cc26
kobbejager <wfremout+github@gmail.com>
2018-07-25 20:36:11 +0200

    fix download of very large files that are stored internally (fpasstru fails)

--- app/core/service/download.php
+++ app/core/service/download.php
@@ -6,32 +6,40 @@ class Download
 {
     public static function path($filePath, $filename = null, $dieOnError = false) 
     {
-        $handle = fopen($filePath, "rb");
+        // NOTE: handle/fpasstru fail on very large files!
+        //$handle = fopen($filePath, "rb");
+        //
+        //if ($handle) {
+        //   if (is_null($filename)) $filename = basename($filePath);
+        //   self::handle($handle, $filename, $dieOnError);
 
-        if ($handle) {
-            if (is_null($filename)) $filename = basename($filePath);
-
-            self::handle($handle, $filename, $dieOnError);
-        } else { // find out the cause of the error
-            // check if file exists, is file and is accessible
-            if (!is_file($filePath)) {
-                eventLog("ERROR", "File not found: $filePath [Core\\Service\\Download]");
-                if ($dieOnError) self::error("404 Not Found");
-                else return false;
-            } elseif (!is_readable($filePath)) {
-                eventLog("ERROR", "File not accessible: $filePath [Core\\Service\\Download]");
-                if ($dieOnError) self::error("403 Forbidden");
-                return false;
-            } else {
-                eventLog("ERROR", "File could not be served: $filePath [Core\\Service\\Download]");
-                if ($dieOnError) self::error("404 Not Found");
-                return false;
-
-                //header('Content-Type: ' . mime_content_type($filePath));
-                //header('Content-Length: ' . filesize($filePath));
-                //readfile($filePath);
-                //die();
-            }    
+        if (file_exists($filePath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="' . basename($filePath) . '"');
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filePath));
+            ob_clean();
+            ob_end_flush();
+            $handle = fopen($filePath, "rb");
+            while (!feof($handle)) {    // serve the file in small bits
+                echo fread($handle, 1000);
+            }
+        } elseif (!is_file($filePath)) { // check if file exists, is file and is accessible
+            eventLog("ERROR", "File not found: $filePath [Core\\Service\\Download]");
+            if ($dieOnError) self::error("404 Not Found");
+            else return false;
+        } elseif (!is_readable($filePath)) {
+            eventLog("ERROR", "File not accessible: $filePath [Core\\Service\\Download]");
+            if ($dieOnError) self::error("403 Forbidden");
+            return false;
+        } else {
+            eventLog("ERROR", "File could not be served: $filePath [Core\\Service\\Download]");
+            if ($dieOnError) self::error("404 Not Found");
+            return false;
         }
     }
 
